4d72a3d70b906b5f14ff99a5dc7cd37534e49458
Mackenzie Noon <mackenziecnoon@gmail.com>
2023-12-12 20:28:21 -0500

    fixing matrix script

--- thread_1/1_pileup/1-1_make_matrix/make_matrix.sh
+++ thread_1/1_pileup/1-1_make_matrix/make_matrix.sh
@@ -3,46 +3,46 @@
 #SBATCH --output=%x_%A_%a.out
 #SBATCH --error=%x_%A_%a.err
 #SBATCH --time=03:00:00
-#SBATCH --mem=128G
+#SBATCH --mem=32G
 #SBATCH --cpus-per-task=3
 #SBATCH --array=1-16
 #SBATCH -p ycga
 
 module load deepTools
 
-# Directories containing BED and BAM files
+# Directories containing BED and BW files
 bed_dir="/home/mcn26/palmer_scratch/joe/data/de_beds"
-bam_dir="/home/mcn26/palmer_scratch/joe/data/bams"
+bw_dir="/home/mcn26/palmer_scratch/joe/data/bigwig"
 
-# Create arrays of BED and BAM files
+# Create arrays of BED and BW files
 bed_files=($(ls $bed_dir/*.bed))
-bam_files=($(ls $bam_dir/*.bam))
+bw_files=($(ls $bw_dir/*.bw))
 
-# Compute the total number of BED and BAM files
+# Compute the total number of BED and BW files
 num_bed_files=${#bed_files[@]}
-num_bam_files=${#bam_files[@]}
+num_bw_files=${#bw_files[@]}
 
-# Calculate the indices for BED and BAM files
-bed_index=$(( ($SLURM_ARRAY_TASK_ID - 1) / num_bam_files ))
-bam_index=$(( ($SLURM_ARRAY_TASK_ID - 1) % num_bam_files ))
+# Calculate the indices for BED and BW files
+bed_index=$(( ($SLURM_ARRAY_TASK_ID - 1) / num_bw_files ))
+bw_index=$(( ($SLURM_ARRAY_TASK_ID - 1) % num_bw_files ))
 
 # Get the file names
 bed_file=${bed_files[$bed_index]}
-bam_file=${bam_files[$bam_index]}
+bw_file=${bw_files[$bw_index]}
 
 # Extract the base names without extensions
 bed_base=$(basename $bed_file .bed)
-bam_base=$(basename $bam_file .bam)
+bw_base=$(basename $bw_file .bw)
 
 # Construct the output matrix file name
-output_matrix="${bed_base}_${bam_base}_matrix.gz"
+output_matrix="${bed_base}_${bw_base}_matrix.gz"
 
 # Run deepTools computematrix
 
 computeMatrix reference-point --referencePoint TSS \
     -b 2000 -a 2000 \
     -R $bed_file \
-    -S $bam_file \
+    -S $bw_file \
     -o $output_matrix
 
-echo "Processed BED file: $bed_file with BAM file: $bam_file. Output: $output_matrix
+echo "Processed BED file: $bed_file with BW file: $bw_file. Output: $output_matrix"
